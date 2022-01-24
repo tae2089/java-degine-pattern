b7414a21cf25fa989bb228a28b49a3d35f1d7be4
tae2089 <tae2089002@gmail.com>
2022-01-24 23:10:38 +0900

    ProductInventory 이름 수정

--- aggregator-microservices/aggregator-service/src/main/java/com/example/aggregatorservice/Aggregator.java
+++ aggregator-microservices/aggregator-service/src/main/java/com/example/aggregatorservice/Aggregator.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 import static java.util.Objects.requireNonNullElse;
 
 @RestController
